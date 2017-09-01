1ad1539382dd36a3896aa088912e4323aaaec183
Mathieu Pavageau <mth.pvg@gmail.com>
2017-09-01 14:49:37 +0200

    chore: first commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# mapzen
+# webpack3-pug
+
+## Quick start
+```bash
+npm install
+```
+
+### Development
+```bash
+npm start
+```
+
+### Production
+```bash
+npm run build
+```
+
+## Add a new page
+- Add a new field in *app/pages/config.json*, for example:
+```json
+{
+  "name": "aboutus",
+  "url": "about-us"
+}
+```
+- Create a file and its directories: *app/pages/aboutus/js/index.js*
---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const pages = buildPages();
+
+
+module.exports = {
+  entry: {
+    index: path.join(__dirname, 'app/pages/index/js/index.js'),
+    secondary: path.join(__dirname, 'app/pages/secondary/js/index.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+        { test: /\.pug$/, loader: "pug-loader" },
+        { test: /\.json$/, loader: "json-loader" },
+        {
+          test: /\.(jpg|jpeg|png|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash:8].[ext]',
+          },
+        },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'app/pages/index/view/index.pug'),
+      filename: 'index.html',
+      inject: 'body',
+      chunks: ['index']
+    }),
+  ].concat(pages),
+};
+
+function buildPages() {
+  const config = require('./app/pages/config.json');
+  return config.map(function (page) {
+    return new HtmlWebpackPlugin({
+      template: path.join(
+        __dirname, 'app/pages/' + page.name+ '/view/index.pug'
+      ),
+      filename: page.url + '/index.html',
+      inject: 'body',
+      chunks: [page.name]
+    })
+  });
+};
