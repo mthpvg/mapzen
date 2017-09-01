54af30f03ab921073e8c9cade7b6844d52d49894
Mathieu Pavageau <mth.pvg@gmail.com>
2017-09-01 14:58:47 +0200

    feat: add elevation on the map

--- app/pages/index/js/index.js
+++ app/pages/index/js/index.js
@@ -1,12 +1,12 @@
 require('../css/index.css');
 
-var sayHello = require('../../../shared/js/say_hello');
-
-console.log(sayHello('index page'));
-
-// Add a Mapzen API key
 L.Mapzen.apiKey = 'mapzen-ztaLGsF';
-// Add a map to the 'map' div
-var map = L.Mapzen.map('map');
-// Set the center of the map to be the San Francisco Bay Area at zoom level 12
+
+var map = L.Mapzen.map('map', {
+  tangramOptions: {
+    scene: L.Mapzen.BasemapStyles.Walkabout
+  }
+})
+
+// Iceland
 map.setView([64.87, -18.40], 7);
